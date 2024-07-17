512411963bf64b4fb99e72855afb07f705d2ba5c
Abdallah Saber <83897573+abdallah065@users.noreply.github.com>
2024-07-18 01:23:32 +0300

    add resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-// import pdf from "../../Assets/../Assets/.pdf";
+
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+import myResume from "../../Assets/Abdallah-Saber.pdf";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -19,29 +22,28 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        {/* <Row className="resume">
-           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
         <Row style={{ justifyContent: "center", position: "relative" }}>
+
           <Button
             variant="primary"
-            href={pdf}
+            href={myResume}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row> */}
-          
+        </Row>
+
+        <Row className="resume">
+          <Document file={myResume} onLoadError={console.error}>
+            <Page pageNumber={1} width={width} />
+          </Document>
+        </Row>
+
+
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          {/* Coming Soon banner */}
-          <div className="coming-soon">
-            <h1>Coming Soon</h1>
-          </div>
         </Row>
       </Container>
     </div>
